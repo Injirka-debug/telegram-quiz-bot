c31c6b76acc81533226d46ea2bd1c8e929e4d4b6
Injirka-debug <tuboltsev_355@mail.ru>
2025-05-07 13:51:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ async def send_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return ConversationHandler.END
 
-    if index == 9:
+    if index == 10:
         await update.message.reply_text(
             "Остался последний шаг - подпишись на наш телеграмм канал t.me/bronislav56",
             disable_web_page_preview=True
